64589e1bfb601a50622ea45a9690751c4319ef04
Alberto Argente del Castillo Garrido <albertoargentedelcastillogarrido@MacBook-Pro-de-Alberto.local>
2022-06-13 22:21:40 +0200

    FLEX-15 Added test for FlexDataset. Renamed test classes. Improvised test methods.

--- tests/data/test_flex_dataset.py
+++ tests/data/test_flex_dataset.py
@@ -7,7 +7,7 @@ from flex.data.flex_dataset import FlexClientDataset
 from flex.data.flex_dataset import FlexDataset
 
 
-class FlexClientDatasetTester(unittest.TestCase):
+class TestFlexClientDataset(unittest.TestCase):
     def test_X_data_property(self):
         X_data = np.random.rand(100).reshape([20,5])
         # y_data = np.random.choice(2, 100)
@@ -17,25 +17,42 @@ class FlexClientDatasetTester(unittest.TestCase):
 
     def test_y_data_property(self):
         X_data = np.random.rand(100).reshape([20,5])
-        y_data = np.random.choice(2, 100)
+        y_data = np.random.choice(2, 20)
         fcd = FlexClientDataset(X_data=X_data, y_data=y_data)
         assert np.array_equal(X_data, fcd.X_data)
         assert np.array_equal(y_data, fcd.y_data)
+        assert X_data.shape[0] == y_data.shape[0]
 
-    def test_X_names_property(self):
+    def test_X_names_property_valid_X_names(self):
         X_data = np.random.rand(100).reshape([20,5])
-        y_data = np.random.choice(2, 100)
+        y_data = np.random.choice(2, 20)
         fcd = FlexClientDataset(X_data=X_data, y_data=y_data)
         X_names = [f"x{i}" for i in range(X_data.shape[1])]
         assert X_names == fcd.X_names
+        assert X_data.shape[0] == y_data.shape[0]
+
+    def test_X_names_property_invalid_X_names(self):
+        X_data = np.random.rand(100).reshape([20,5])
+        y_data = np.random.choice(2, 20)
+        fcd = FlexClientDataset(X_data=X_data, y_data=y_data)
+        X_names = [f"x{i}" for i in range(X_data.shape[1]+4)]
+        assert X_names != fcd.X_names
+        assert X_data.shape[0] == y_data.shape[0]
 
     def test_y_names_property(self):
         X_data = np.random.rand(100).reshape([20,5])
-        y_data = np.random.choice(2, 100)
+        y_data = np.random.choice(2, 20)
         fcd = FlexClientDataset(X_data=X_data, y_data=y_data)
         y_names = [f"class_{c}" for c in np.unique(y_data)]
         assert y_names == fcd.y_names
+        assert X_data.shape[0] == y_data.shape[0]
 
 
-class FlexDatasetTester(FlexDataset):
-    pass
+class TestFlexDataset(unittest.TestCase):
+    def test_get_method(self):
+        X_data = np.random.rand(100).reshape([20,5])
+        y_data = np.random.choice(2, 20)
+        fcd = FlexClientDataset(X_data=X_data, y_data=y_data)
+        flex_data = FlexDataset()
+        flex_data['client_1'] = fcd
+        assert flex_data['client_1'] == fcd
