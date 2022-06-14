b2f53bed30e8030b7be4c5cf9aac5a78047c6807
Juan <juan@MacBook-Air-de-Juan.local>
2022-06-14 12:07:29 +0200

    Added proper linting to the project by using Trunk.

--- flex/data/flex_dataset.py
+++ flex/data/flex_dataset.py
@@ -1,5 +1,5 @@
 from collections import UserDict
-from typing import Optional, List, Sequence, Any
+from typing import Any, List, Optional, Sequence
 
 import numpy as np
 import numpy.typing as npt
@@ -7,7 +7,7 @@ import numpy.typing as npt
 
 class FlexClientDataset:
     """Class used to represent the dataset from a client in a Federated Learning enviroment.
-    
+
     Attributes
     ----------
     X_data: numpy.typing.NDArray
@@ -20,16 +20,30 @@ class FlexClientDataset:
     y_names: Optional[List[str]]
         A list of strings containing the class names. Default None.
     """
-    __slots__ = ('__X_data', '__y_data', '__X_names', '__y_names')
-    
-    def __init__(self, X_data: npt.NDArray, y_data: Optional[Sequence[npt._SupportsArray[np.dtype[Any]]]] = None, 
-                 X_names: Optional[List[str]] = None, y_names: Optional[List[str]] = None) -> None:
+
+    __slots__ = ("__X_data", "__y_data", "__X_names", "__y_names")
+
+    def __init__(
+        self,
+        X_data: npt.NDArray,
+        y_data: Optional[Sequence[npt._SupportsArray[np.dtype[Any]]]] = None,
+        X_names: Optional[List[str]] = None,
+        y_names: Optional[List[str]] = None,
+    ) -> None:
 
         self.__X_data = X_data
         self.__y_data = y_data
 
-        self.__X_names = X_names if (X_names and len(X_names) == X_data.shape[1]) else None
-        self.__y_names = y_names if y_names and y_data is not None and (len(np.unique(y_data)) == len(np.unique(y_names))) else None
+        self.__X_names = (
+            X_names if (X_names and len(X_names) == X_data.shape[1]) else None
+        )
+        self.__y_names = (
+            y_names
+            if y_names
+            and y_data is not None
+            and (len(np.unique(y_data)) == len(np.unique(y_names)))
+            else None
+        )
 
     @property
     def X_data(self):
@@ -78,7 +92,7 @@ class FlexDataset(UserDict):
     def __setitem__(self, key: str, item: FlexClientDataset) -> None:
         self.data[key] = item
 
-    def get(self, key:str, default:Optional[Any]=None) -> Any:
+    def get(self, key: str, default: Optional[Any] = None) -> Any:
         try:
             return self[key]
         except KeyError:
--- tests/data/test_flex_dataset.py
+++ tests/data/test_flex_dataset.py
@@ -1,36 +1,33 @@
-import pytest
 import unittest
 
 import numpy as np
 
-from flex.data.flex_dataset import FlexClientDataset
-from flex.data.flex_dataset import FlexDataset
+from flex.data.flex_dataset import FlexClientDataset, FlexDataset
 
 
 class TestFlexClientDataset(unittest.TestCase):
     def test_X_data_property(self):
-        X_data = np.random.rand(100).reshape([20,5])
-        # y_data = np.random.choice(2, 100)
+        X_data = np.random.rand(100).reshape([20, 5])
         fcd = FlexClientDataset(X_data=X_data)
         assert np.array_equal(X_data, fcd.X_data)
 
     def test_X_data_property_setter(self):
-        X_data = np.random.rand(100).reshape([20,5])
+        X_data = np.random.rand(100).reshape([20, 5])
         fcd = FlexClientDataset(X_data=X_data)
         assert np.array_equal(X_data, fcd.X_data)
-        X_data = np.random.rand(100).reshape([20,5])
+        X_data = np.random.rand(100).reshape([20, 5])
         fcd.X_data = X_data
         assert np.array_equal(X_data, fcd.X_data)
 
     def test_y_data_property(self):
-        X_data = np.random.rand(100).reshape([20,5])
+        X_data = np.random.rand(100).reshape([20, 5])
         y_data = np.random.choice(2, 20)
         fcd = FlexClientDataset(X_data=X_data, y_data=y_data)
         assert np.array_equal(y_data, fcd.y_data)
         assert np.array_equal(y_data, fcd.y_data)
 
     def test_y_data_property_setter(self):
-        X_data = np.random.rand(100).reshape([20,5])
+        X_data = np.random.rand(100).reshape([20, 5])
         y_data = np.random.choice(2, 20)
         fcd = FlexClientDataset(X_data=X_data, y_data=y_data)
         assert np.array_equal(y_data, fcd.y_data)
@@ -39,7 +36,7 @@ class TestFlexClientDataset(unittest.TestCase):
         assert np.array_equal(y_data, fcd.y_data)
 
     def test_X_names_property_valid_X_names(self):
-        X_data = np.random.rand(100).reshape([20,5])
+        X_data = np.random.rand(100).reshape([20, 5])
         y_data = np.random.choice(2, 20)
         X_names = [f"x{i}" for i in range(X_data.shape[1])]
         fcd = FlexClientDataset(X_data=X_data, y_data=y_data, X_names=X_names)
@@ -47,15 +44,15 @@ class TestFlexClientDataset(unittest.TestCase):
         assert X_data.shape[0] == y_data.shape[0]
 
     def test_X_names_property_invalid_X_names(self):
-        X_data = np.random.rand(100).reshape([20,5])
+        X_data = np.random.rand(100).reshape([20, 5])
         y_data = np.random.choice(2, 20)
         fcd = FlexClientDataset(X_data=X_data, y_data=y_data)
-        X_names = [f"x{i}" for i in range(X_data.shape[1]+4)]
+        X_names = [f"x{i}" for i in range(X_data.shape[1] + 4)]
         assert X_names != fcd.X_names
         assert X_data.shape[0] == y_data.shape[0]
 
     def test_X_names_property_setter(self):
-        X_data = np.random.rand(100).reshape([20,5])
+        X_data = np.random.rand(100).reshape([20, 5])
         y_data = np.random.choice(2, 20)
         fcd = FlexClientDataset(X_data=X_data, y_data=y_data)
         X_names = [f"x{i}" for i in range(X_data.shape[1])]
@@ -63,7 +60,7 @@ class TestFlexClientDataset(unittest.TestCase):
         assert X_names == fcd.X_names
 
     def test_y_names_property_valid_y_names(self):
-        X_data = np.random.rand(100).reshape([20,5])
+        X_data = np.random.rand(100).reshape([20, 5])
         y_data = np.random.choice(2, 20)
         y_names = [f"class_{c}" for c in np.unique(y_data)]
         fcd = FlexClientDataset(X_data=X_data, y_data=y_data, y_names=y_names)
@@ -71,24 +68,25 @@ class TestFlexClientDataset(unittest.TestCase):
         assert y_names == fcd.y_names
 
     def test_empty_y_names_y_data(self):
-        X_data = np.random.rand(100).reshape([20,5])
+        X_data = np.random.rand(100).reshape([20, 5])
         fcd = FlexClientDataset(X_data=X_data)
         assert fcd.y_names is None
 
     def test_y_names_property_setter(self):
-        X_data = np.random.rand(100).reshape([20,5])
+        X_data = np.random.rand(100).reshape([20, 5])
         y_data = np.random.choice(2, 20)
         fcd = FlexClientDataset(X_data=X_data, y_data=y_data)
         y_names = [f"class_{c}" for c in np.unique(y_data)]
         fcd.y_names = y_names
         assert y_names == fcd.y_names
 
+
 class TestFlexDataset(unittest.TestCase):
     def test_get_method(self):
-        X_data = np.random.rand(100).reshape([20,5])
+        X_data = np.random.rand(100).reshape([20, 5])
         y_data = np.random.choice(2, 20)
         fcd = FlexClientDataset(X_data=X_data, y_data=y_data)
         flex_data = FlexDataset()
-        flex_data['client_1'] = fcd
-        assert flex_data['client_1'] == fcd
-        assert flex_data.get('client_2') is None
+        flex_data["client_1"] = fcd
+        assert flex_data["client_1"] == fcd
+        assert flex_data.get("client_2") is None
