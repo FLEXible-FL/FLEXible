f87fee6a67a613bfec1d21f4c64565ebc0720485
Alberto Argente del Castillo Garrido <albertoargentedelcastillogarrido@MacBook-Pro-de-Alberto.local>
2022-06-20 11:51:34 +0200

    Added class for making the federated distribution. Still needs to be completed with other tasks.

---
+++ flex/data/flex_data_distribution.py
@@ -0,0 +1,32 @@
+from flex.data.flex_dataset import FederatedFlexDataObject, FlexDataObject
+
+
+class FlexDataDistribution(object):
+    __create_key = object()
+
+    def __init__(self, create_key) -> None:
+        assert (
+            create_key == FlexDataDistribution.__create_key
+        ), """FlexDataDistribution objects must be created using FlexDataDistribution.from_state or
+        FlexDataDistribution.iid_distribution"""
+
+    @classmethod
+    def from_state(cls, data: FlexDataObject = None, state=None):
+        """This function prepare the data from a centralized data structure to a federated one.
+        It will run diffetent functions to federate the data.
+
+        Args:
+            data (FlexDataObject): Centralized dataset represected as a FlexDataObject.
+            state (FlexState): FlexState with the configuration to federate the dataset.
+
+        Returns:
+            federated_dataset (FederatedFlexDataObject): The dataset federated.
+        """
+        # TODO: Once FlexState is finished, continue with the development of the class.
+        return FederatedFlexDataObject()
+
+    @classmethod
+    def iid_distribution(cls, data, n_clients):
+        # TODO: Once FlexState is finished, and other functions to create the FlexDataset
+        # are finished too, continue with this class.
+        return FederatedFlexDataObject()
