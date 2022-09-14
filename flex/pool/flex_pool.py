b1f6a8d3739a7d0a75535eed14d67efefd1d2a9b
Alberto Argente del Castillo Garrido <albertoargentedelcastillogarrido@MacBook-Pro-de-Alberto.local>
2022-09-14 07:56:34 +0200

    Documentation added to some FlexPool methods.

--- flex/pool/flex_pool.py
+++ flex/pool/flex_pool.py
@@ -28,6 +28,17 @@ class FlexPool:
 
     @classmethod
     def check_compatibility(cls, src_pool, dst_pool):
+        """Method to check the compatibility between two different pools. 
+        This method is used by the map_procedure function to check if the
+        function to apply from the source pool to the destiny pool can be done.
+
+        Args:
+            src_pool (FlexPool): Source pool. Pool that will send the message.
+            dst_pool (FlexPool): Destiny pool. Pool that will recieve the message.
+
+        Returns:
+            bool: True if pools are compatible. False in other case.
+        """
         return all(
             FlexRoleManager.check_compatibility(src, dst)
             for _, src in src_pool._actors.items()
@@ -35,6 +46,30 @@ class FlexPool:
         )
 
     def map_procedure(self, func: Callable, dst_pool: FlexPool, *args, **kwargs):
+        """Method used to send messages from one pool to another. The pool using
+        this method is the source pool, and it will send a message, apply a function,
+        to the destiny pool. The pool sends a message in other to complete a round
+        in the Federated Learning (FL) paradigm, so, some examples of the messages
+        that will be used by the pools are: 
+        - send_model: Aggregators send the model to the server when the aggregation is done.
+        - aggregation_step: Clients send the model to the aggregator so it can apply the
+        aggregation mechanism given.
+        - deploy_model: Server sends the global model to the clients once the weights has
+        been aggregated.
+        - init_model: Server sends the model to train during the learning phase, so the
+        clients can initialize it. This is a particular case from the deploy_model case.
+
+        Args:
+            func (Callable): Message to pass from the source pool (self) to the destinity pool (dst_pool)
+            dst_pool (FlexPool): Pool that will recieve the message from the source pool (self).
+
+        Raises:
+            ValueError: This method raises and error if the pools aren't
+
+        Returns:
+            _type_: The result of applying the function (func) from the source pool (self) to the
+            destinty pool (dst_pool).
+        """
         if FlexPool.check_compatibility(self, dst_pool):
             return func(self._models, dst_pool._models, *args, **kwargs)
         else:
@@ -74,14 +109,29 @@ class FlexPool:
 
     @functools.cached_property
     def clients(self):
+        """Property to get all the clients available in a pool
+
+        Returns:
+            FlexPool: Pool containing all the clients from a pool
+        """
         return self.filter(lambda a, b: FlexRoleManager.is_client(b))
 
     @functools.cached_property
     def aggregators(self):
+        """Property to get all the aggregator available in a pool.
+
+        Returns:
+            FlexPool: Pool containing all the aggregators from a pool
+        """
         return self.filter(lambda a, b: FlexRoleManager.is_aggregator(b))
 
     @functools.cached_property
     def servers(self):
+        """Property to get all the servers available in a pool.
+
+        Returns:
+            FlexPool: Pool containing all the servers from a pool
+        """
         return self.filter(lambda a, b: FlexRoleManager.is_server(b))
 
     def validate(self):
@@ -128,6 +178,22 @@ class FlexPool:
     def client_server_architecture(
         cls, fed_dataset: FlexDataset, dropout_rate: float = None
     ):
+        """Method to create a client-server architeture for a FlexDataset given.
+        This functions is used when you have a FlexDataset and you want to start 
+        the learning phase following a traditional client-server architecture.
+        
+        This method will assing to each id from the FlexDataset the client-role, 
+        and will create a new actor that will be the server-aggregator that will
+        orchestrate the learning phase.
+
+        Args:
+            fed_dataset (FlexDataset): Federated dataset used to train a model.
+            dropout_rate (float, optional): Percentage of clients that will drop
+            before the training phase starts. Defaults to None.
+
+        Returns:
+            FlexPool: A FlexPool with the assigned roles for a client-server architecture.
+        """
         actors = FlexActors(
             {actor_id: FlexRole.client for actor_id in fed_dataset.keys()}
         )
@@ -141,6 +207,22 @@ class FlexPool:
 
     @classmethod
     def p2p_architecture(cls, fed_dataset: FlexDataset, dropout_rate: float = None):
+        """Method to create a peer-to-peer (p2p) architecture for a FlexDataset given.
+        This method is used when you have a FlexDataset and you want to start the
+        learning phase following a p2p architecture.
+        
+        This method will assing all roles (client-aggregator-server) to every id from
+        the FlexDataset, so each participant in the learning phase can act as client,
+        aggregator and server.
+
+        Args:
+            fed_dataset (FlexDataset): Federated dataset used to train a model.
+            dropout_rate (float, optional): Percentage of clients that will drop
+            before the training phase starts. Defaults to None.
+
+        Returns:
+            FlexPool: A FlexPool with the assigned roles for a p2p architecture.
+        """
         return cls(
             flex_data=fed_dataset,
             flex_actors=cls.__create_actors_all_privileges(fed_dataset.keys()),
@@ -150,6 +232,15 @@ class FlexPool:
 
     @classmethod
     def __create_actors_all_privileges(cls, actors_ids):
+        """Method that initialize the actors for the pool with all the privileges
+        available. This method must be used only when creating a p2p-architecture.
+
+        Args:
+            actors_ids (str): The IDs that identify every actor in the pool.
+
+        Returns:
+            FlexActors: All the actors for the pool with all the privileges.
+        """
         return FlexActors(
             {actor_id: FlexRole.server_aggregator_client for actor_id in actors_ids}
         )
