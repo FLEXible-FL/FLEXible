c629b141c521ba5c15ae7d224ee64c4affe43fd8
xehartnort <xehartnort@gmail.com>
2022-09-21 10:42:22 +0200

    fix circular imports

--- flex/data/__init__.py
+++ flex/data/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from flex.data.flex_dataset import FlexDataObject
+from flex.data.flex_data_object import FlexDataObject
 from flex.data.flex_dataset import FlexDataset
 from flex.data.flex_dataset_config import FlexDatasetConfig
 from flex.data.flex_data_distribution import FlexDataDistribution
