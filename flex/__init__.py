3fe8686bd1bcee347ff41a48f33cfcd49ce91094
Alberto Argente del Castillo Garrido <albertoargentedelcastillogarrido@MacBook-Pro-de-Alberto.local>
2022-09-27 12:05:48 +0200

    Notebooks

--- flex/__init__.py
+++ flex/__init__.py
@@ -1,6 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function
 
-from flex import data
-from flex import pool
+from flex import data, pool
--- flex/pool/__init__.py
+++ flex/pool/__init__.py
@@ -1,8 +1,12 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function
 
+<<<<<<< Updated upstream
 from flex.pool.flex_pool import FlexPool
 from flex.pool.actors import FlexRole
 from flex.pool.actors import FlexRoleManager
 from flex.pool.actors import FlexActors
+=======
+from flex.pool.actors import FlexActors, FlexRole, FlexRoleManager
+from flex.pool.flex_model import FlexModel
+from flex.pool.flex_pool import FlexPool
+>>>>>>> Stashed changes
