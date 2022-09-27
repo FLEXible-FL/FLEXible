88abc9cfafc2b8872988389768c6a95a3ed12c5c
Alberto Argente del Castillo Garrido <albertoargentedelcastillogarrido@MacBook-Pro-de-Alberto.local>
2022-09-27 14:03:59 +0200

    Notebooks but have errors

--- flex/data/__init__.py
+++ flex/data/__init__.py
@@ -1,11 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
-<<<<<<< Updated upstream
-from flex.data.flex_dataset import FlexDataObject
-=======
 from flex.data.flex_data_distribution import FlexDataDistribution
 from flex.data.flex_data_object import FlexDataObject
->>>>>>> Stashed changes
 from flex.data.flex_dataset import FlexDataset
 from flex.data.flex_dataset_config import FlexDatasetConfig
 from flex.data.flex_preprocessing_utils import normalize, one_hot_encoding
--- flex/data/flex_dataset.py
+++ flex/data/flex_dataset.py
@@ -1,59 +1,13 @@
 from collections import UserDict
 from copy import deepcopy
-from dataclasses import dataclass, field
 from functools import partial
 from multiprocessing import Pool
 from typing import Any, Callable, Hashable, List, Optional
 
 import numpy.typing as npt
 
+from flex.data.flex_data_object import FlexDataObject
 from flex.data.flex_preprocessing_utils import normalize, one_hot_encoding
-
-
-@dataclass(frozen=True)
-class FlexDataObject:
-    """Class used to represent the dataset from a client in a Federated Learning enviroment.
-
-    Attributes
-    ----------
-    _X_data: numpy.typing.ArrayLike
-        A numpy.array containing the data for the client.
-    _y_data: numpy.typing.ArrayLike
-        A numpy.array containing the labels for the training data. Can be None if working
-        on an unsupervised learning task. Default None.
-    """
-
-    X_data: npt.NDArray = field(init=True)
-    y_data: Optional[npt.NDArray] = field(default=None, init=True)
-
-    def __len__(self):
-        return len(self.X_data)
-
-    def __getitem__(self, pos):
-        if self.y_data is None:
-            return FlexDataObject(self.X_data[pos], None)
-        else:
-            return FlexDataObject(
-                self.X_data[pos],
-                self.y_data[pos[0]] if isinstance(pos, tuple) else self.y_data[pos],
-            )
-
-    def __iter__(self):
-        return zip(
-            self.X_data,
-            self.y_data if self.y_data is not None else [None] * len(self.X_data),
-        )
-
-    def validate(self):
-        """Function that checks whether the object is correct or not."""
-        if self.y_data is not None and len(self.X_data) != len(self.y_data):
-            raise ValueError(
-                f"X_data and y_data must have equal lenght. X_data has {len(self.X_data)} elements and y_data has {len(self.y_data)} elements."
-            )
-        if self.y_data is not None and self.y_data.ndim > 1:
-            raise ValueError(
-                "y_data is multidimensional and we only support unidimensional labels."
-            )
 
 
 class FlexDataset(UserDict):
