32677497c711ac5bd5e19492b8c4dc6ef79c9355
Alberto Argente del Castillo Garrido <albertoargentedelcastillogarrido@MacBook-Pro-de-Alberto.local>
2022-10-10 15:38:21 +0200

    Changed how to load the data in the torch notebook, now using from_torchtext_dataset/from_huggingface_dataset.

--- flex/data/__init__.py
+++ flex/data/__init__.py
@@ -9,5 +9,4 @@ from flex.data.flex_data_distribution import FlexDataDistribution
 from flex.data.flex_preprocessing_utils import normalize
 from flex.data.flex_preprocessing_utils import one_hot_encoding
 from flex.data.pluggable_datasets import PluggableDatasetsHuggingFace
-from flex.data.pluggable_datasets import PluggableDatasetsTensorFlow
 from flex.data.pluggable_datasets import PluggableDatasetsTorchText
