5dd559d85b55bcf9b8c27cb77094f4df12be4e86
Alberto Argente del Castillo Garrido <albertoargentedelcastillogarrido@MacBook-Pro-de-Alberto.local>
2022-10-13 13:21:40 +0200

    Changed name of primitive file on the init file

--- flex/pool/__init__.py
+++ flex/pool/__init__.py
@@ -7,10 +7,10 @@ from flex.pool.actors import FlexRole
 from flex.pool.actors import FlexRoleManager
 from flex.pool.actors import FlexActors
 from flex.pool.flex_model import FlexModel
-from flex.pool.primitive_functions import initialize_server_model
-from flex.pool.primitive_functions import deploy_global_model_to_clients
-from flex.pool.primitive_functions import deploy_model_to_clients
-from flex.pool.primitive_functions import collect_weights
-from flex.pool.primitive_functions import aggregate_weights
-from flex.pool.primitive_functions import evaluate_model
-from flex.pool.primitive_functions import train
+from flex.pool.flex_primitives import initialize_server_model
+from flex.pool.flex_primitives import deploy_global_model_to_clients
+from flex.pool.flex_primitives import deploy_model_to_clients
+from flex.pool.flex_primitives import collect_weights
+from flex.pool.flex_primitives import aggregate_weights
+from flex.pool.flex_primitives import evaluate_model
+from flex.pool.flex_primitives import train
