6c0120e7895c2eed08aebd4f96b4e6c744eba083
Alberto Argente del Castillo Garrido <albertoargentedelcastillogarrido@MacBook-Pro-de-Alberto.local>
2022-10-19 17:12:59 +0200

    Linter applied to delete unsused imports.

--- tests/pool/test_flex_primitives.py
+++ tests/pool/test_flex_primitives.py
@@ -1,63 +1,59 @@
-import copy
 import unittest
 
 import pytest
-from sklearn.datasets import load_iris
-from sklearn.metrics import accuracy_score
 
-from flex.data import FlexDataDistribution
-from flex.data.flex_dataset import FlexDataObject
-
-from flex.pool.flex_primitives import (
-    initialize_server_model_tf,
-    deploy_server_model_to_clients_tf,
-    train_tf,
-    collect_weithts_tf,
-    collect_weights_pt,
-    set_aggregated_weights_tf,
-    set_aggregated_weights_pt,
-    evaluate_server_model_tf
-)
-
-from flex.pool.flex_aggregators import fed_avg
-
-from flex.pool.flex_model import FlexModel
-from flex.pool.flex_pool import FlexPool
 from flex.data import FlexDataDistribution, FlexDatasetConfig
+from flex.pool.flex_aggregators import fed_avg
+from flex.pool.flex_pool import FlexPool
+from flex.pool.flex_primitives import (  # collect_weights_pt,; set_aggregated_weights_pt,
+    collect_weithts_tf,
+    deploy_server_model_to_clients_tf,
+    evaluate_server_model_tf,
+    initialize_server_model_tf,
+    set_aggregated_weights_tf,
+    train_tf,
+)
 
 
 class TestFlexPoolPrimitives(unittest.TestCase):
     @pytest.fixture(autouse=True)
     def _fixture_ag_news_dataset(self):
         from datasets import load_dataset
+
         config = FlexDatasetConfig(
-                seed=0,
-                n_clients=2,
-                replacement=False,
-                client_names=["client_0", "client_1"],
-            )
+            seed=0,
+            n_clients=2,
+            replacement=False,
+            client_names=["client_0", "client_1"],
+        )
         self.config = config
-        train_data, test_data = load_dataset('ag_news', split=['train', 'test'])
+        train_data, test_data = load_dataset("ag_news", split=["train", "test"])
         self.test_data = test_data
-        X_columns = 'text'
-        label_column = 'label'
-        self.f_imdb = FlexDataDistribution.from_config_with_huggingface_dataset(train_data, self.config,
-                                                                                X_columns, label_column)
+        X_columns = "text"
+        label_column = "label"
+        self.f_imdb = FlexDataDistribution.from_config_with_huggingface_dataset(
+            train_data, self.config, X_columns, label_column
+        )
 
     def test_primitives_tf(self):
+        import numpy as np
         import tensorflow as tf
         import tensorflow_hub as hub
-        import numpy as np
+
         def define_model(*args):
             model = "https://tfhub.dev/google/nnlm-en-dim50/2"
-            hub_layer = hub.KerasLayer(model, input_shape=[], dtype=tf.string, trainable=True)
+            hub_layer = hub.KerasLayer(
+                model, input_shape=[], dtype=tf.string, trainable=True
+            )
             model = tf.keras.Sequential()
             model.add(hub_layer)
-            model.add(tf.keras.layers.Dense(16, activation='relu'))
+            model.add(tf.keras.layers.Dense(16, activation="relu"))
             model.add(tf.keras.layers.Dense(1))
-            model.compile(optimizer='adam',
-                            loss=tf.losses.BinaryCrossentropy(from_logits=True),
-                            metrics=[tf.metrics.BinaryAccuracy(threshold=0.0, name='accuracy')])
+            model.compile(
+                optimizer="adam",
+                loss=tf.losses.BinaryCrossentropy(from_logits=True),
+                metrics=[tf.metrics.BinaryAccuracy(threshold=0.0, name="accuracy")],
+            )
             return model
 
         p = FlexPool.client_server_architecture(
@@ -67,24 +63,36 @@ class TestFlexPoolPrimitives(unittest.TestCase):
         reference_model_params = reference_model.get_weights()
         p.servers.map(deploy_server_model_to_clients_tf, p.clients)
         assert all(
-            np.all(np.equal(p._models[k]["model"].get_weights()[0], reference_model_params[0]))
+            np.all(
+                np.equal(
+                    p._models[k]["model"].get_weights()[0], reference_model_params[0]
+                )
+            )
             for k in p.actor_ids
         )
-        # Train the model
+        # Train the model
         p.clients.map(train_tf, batch_size=512, epochs=1)
-        # Collect weights
+        # Collect weights
         p.aggregators.map(collect_weithts_tf, p.clients)
         # Aggregate weights
         p.aggregators.map(fed_avg)
-        assert np.any(np.not_equal(p._models["server"]["aggregated_weights"][0], reference_model_params[0]))
+        assert np.any(
+            np.not_equal(
+                p._models["server"]["aggregated_weights"][0], reference_model_params[0]
+            )
+        )
         # Transfer weights from aggregators to servers
         p.aggregators.map(set_aggregated_weights_tf, p.servers)
         # Deploy new model to clients
         p.servers.map(deploy_server_model_to_clients_tf, p.clients)
-        reference_value = p._models["server"]['model'].get_weights()
+        reference_value = p._models["server"]["model"].get_weights()
         assert all(
             np.all(np.equal(p._models[k]["model"].get_weights()[0], reference_value[0]))
             for k in p.actor_ids
         )
-        result = p.servers.map(evaluate_server_model_tf, test_data=self.test_data['text'], test_labels=self.test_data['label'])
+        result = p.servers.map(
+            evaluate_server_model_tf,
+            test_data=self.test_data["text"],
+            test_labels=self.test_data["label"],
+        )
         print(result)
