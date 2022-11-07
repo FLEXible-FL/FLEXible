2b8c4f4beffa0dae39a626bb2a5b4ac0ebcedab0
xehartnort <xehartnort@gmail.com>
2022-11-07 10:39:07 +0100

    added test for flex_dataset_config refactor

--- flex/data/flex_dataset_config.py
+++ flex/data/flex_dataset_config.py
@@ -5,6 +5,12 @@ import numpy as np
 import numpy.typing as npt
 
 
+class InvalidConfig(ValueError):
+    """Raised when the input config is wrong"""
+
+    pass
+
+
 @dataclass
 class FlexDatasetConfig:
     """Class used to represent a configuration to federate a centralized dataset.
@@ -12,15 +18,15 @@ class FlexDatasetConfig:
 
     | Options compatibility   | **n_clients** | **client_names** | **weights** | **weights_per_class** | **replacement** | **classes_per_client** | **features_per_client** | **indexes_per_client** | **group_by_feature** |
     |-------------------------|---------------|------------------|-------------|-----------------------|-----------------|------------------------|-------------------------|------------------------|----------------------|
-    | **n_clients**           | -             | Y                | Y           | Y                     | Y               | Y                      | Y                       | Y                      | N                    |
-    | **client_names**        | Y             | -                | Y           | Y                     | Y               | Y                      | Y                       | Y                      | N                    |
-    | **weights**             | Y             | Y                | -           | N                     | Y               | Y                      | Y                       | N                      | N                    |
-    | **weights_per_class**   | Y             | Y                | N           | -                     | Y               | N                      | N                       | N                      | N                    |
-    | **replacement**         | Y             | Y                | Y           | Y                     | -               | Y                      | Y                       | N                      | N                    |
-    | **classes_per_client**  | Y             | Y                | Y           | N                     | Y               | -                      | N                       | N                      | N                    |
-    | **features_per_client** | Y             | Y                | Y           | N                     | Y               | N                      | -                       | N                      | N                    |
-    | **indexes_per_client**  | Y             | Y                | N           | N                     | N               | N                      | N                       | -                      | N                    |
-    | **group_by_feature**    | N             | N                | N           | N                     | N               | N                      | N                       | N                      | -                    |
+    | **n_clients**           | -             | Y                | Y           | Y                     | Y               | Y                      | Y                       | N                      | N                    |
+    | **client_names**        | -             | -                | Y           | Y                     | Y               | Y                      | Y                       | Y                      | N                    |
+    | **weights**             | -             | -                | -           | N                     | Y               | Y                      | Y                       | N                      | N                    |
+    | **weights_per_class**   | -             | -                | -           | -                     | Y               | N                      | N                       | N                      | N                    |
+    | **replacement**         | -             | -                | -           | -                     | -               | Y                      | Y                       | N                      | N                    |
+    | **classes_per_client**  | -             | -                | -           | -                     | -               | -                      | N                       | N                      | N                    |
+    | **features_per_client** | -             | -                | -           | -                     | -               | -                      | -                       | N                      | N                    |
+    | **indexes_per_client**  | -             | -                | -           | -                     | -               | -                      | -                       | -                      | N                    |
+    | **group_by_feature**    | -             | -                | -           | -                     | -               | -                      | -                       | -                      | -                    |
 
     Attributes
     ----------
@@ -63,52 +69,37 @@ class FlexDatasetConfig:
     indexes_per_client: Optional[npt.NDArray] = None
     group_by_feature: Optional[int] = None
 
-    def __check_incompatibility(self, dict, option1, option2):
-        if dict[option1] is not None and dict[option2] is not None:
-            raise ValueError(
-                f"Invalid configuration: Options {option1} and {option2} are incompatible, please provide only one."
+    def _check_incomp(self, dict, option1, option2):
+        """This function checks if two options are compatible, if not it raises and exception"""
+        cond1 = dict[option1] is not None if option1 != "replacement" else dict[option1]
+        cond2 = dict[option2] is not None if option2 != "replacement" else dict[option2]
+        if cond1 and cond2:
+            raise InvalidConfig(
+                f"Options {option1} and {option2} are incompatible, please provide only one."
             )
 
     def validate(self):
         """This function checks whether the configuration to federate a dataset is correct."""
         self_dict = asdict(self)
         # By default every option is compatible, therefore we only specify incompatibilities
-        self.__check_incompatibility(self_dict, "n_clients", "group_by_feature")
-        self.__check_incompatibility(self_dict, "client_names", "group_by_feature")
-        self.__check_incompatibility(self_dict, "weights", "group_by_feature")
-        self.__check_incompatibility(self_dict, "weights", "weights_per_class")
-        self.__check_incompatibility(self_dict, "weights", "indexes_per_client")
-        self.__check_incompatibility(self_dict, "weights", "group_by_feature")
-        self.__check_incompatibility(
-            self_dict, "weights_per_class", "indexes_per_client"
-        )
-        self.__check_incompatibility(
-            self_dict, "weights_per_class", "classes_per_client"
-        )
-        self.__check_incompatibility(
-            self_dict, "weights_per_class", "features_per_client"
-        )
-        self.__check_incompatibility(
-            self_dict, "weights_per_class", "indexes_per_client"
-        )
-        self.__check_incompatibility(self_dict, "weights_per_class", "group_by_feature")
-        self.__check_incompatibility(self_dict, "replacement", "indexes_per_client")
-        self.__check_incompatibility(self_dict, "replacement", "group_by_feature")
-        self.__check_incompatibility(
-            self_dict, "classes_per_client", "features_per_client"
-        )
-        self.__check_incompatibility(
-            self_dict, "classes_per_client", "indexes_per_client"
-        )
-        self.__check_incompatibility(
-            self_dict, "classes_per_client", "group_by_feature"
-        )
-        self.__check_incompatibility(
-            self_dict, "features_per_client", "indexes_per_client"
-        )
-        self.__check_incompatibility(
-            self_dict, "features_per_client", "group_by_feature"
-        )
+        self._check_incomp(self_dict, "n_clients", "group_by_feature")
+        self._check_incomp(self_dict, "client_names", "group_by_feature")
+        self._check_incomp(self_dict, "weights", "group_by_feature")
+        self._check_incomp(self_dict, "weights", "weights_per_class")
+        self._check_incomp(self_dict, "weights", "indexes_per_client")
+        self._check_incomp(self_dict, "weights", "group_by_feature")
+        self._check_incomp(self_dict, "weights_per_class", "indexes_per_client")
+        self._check_incomp(self_dict, "weights_per_class", "classes_per_client")
+        self._check_incomp(self_dict, "weights_per_class", "features_per_client")
+        self._check_incomp(self_dict, "weights_per_class", "indexes_per_client")
+        self._check_incomp(self_dict, "weights_per_class", "group_by_feature")
+        self._check_incomp(self_dict, "replacement", "indexes_per_client")
+        self._check_incomp(self_dict, "replacement", "group_by_feature")
+        self._check_incomp(self_dict, "classes_per_client", "features_per_client")
+        self._check_incomp(self_dict, "classes_per_client", "indexes_per_client")
+        self._check_incomp(self_dict, "classes_per_client", "group_by_feature")
+        self._check_incomp(self_dict, "features_per_client", "indexes_per_client")
+        self._check_incomp(self_dict, "features_per_client", "group_by_feature")
 
         self.__validate_clients_and_weights()
         if self.indexes_per_client is not None:
@@ -116,33 +107,35 @@ class FlexDatasetConfig:
         elif self.classes_per_client is not None:
             self.__validate_classes_per_client()
         elif self.features_per_client is not None:
-            self.__validate_features_per_class()
+            self.__validate_features_per_client()
 
     def __validate_indexes_per_client(self):
         if len(self.indexes_per_client) != self.n_clients:
-            raise ValueError(
+            raise InvalidConfig(
                 "The number of provided clients should equal the length of indexes per client."
             )
 
     def __validate_clients_and_weights(self):
         if self.n_clients < 2:
-            raise ValueError(
+            raise InvalidConfig(
                 "The number of clients must be greater or equal to 2. Default is 2"
             )
 
         if self.client_names is not None and self.n_clients > len(self.client_names):
-            raise ValueError(
+            raise InvalidConfig(
                 "The number of named clients, client_names, can not be greater than the number of clients, n_clients"
             )
 
         if self.weights is not None and self.n_clients != len(self.weights):
-            raise ValueError("The number of weights must equal the number of clients.")
+            raise InvalidConfig(
+                "The number of weights must equal the number of clients."
+            )
 
         if (
             self.weights_per_class is not None
             and len(np.asarray(self.weights_per_class).shape) != 2
         ):
-            raise ValueError(
+            raise InvalidConfig(
                 (
                     "weights_per_class must be a two dimensional array where the first dimension is the number of clients and the second is the number of classes of the dataset to federate."
                 )
@@ -151,41 +144,45 @@ class FlexDatasetConfig:
         if self.weights_per_class is not None and self.n_clients != len(
             self.weights_per_class
         ):
-            raise ValueError(
+            raise InvalidConfig(
                 "The length of weights_per_class must equal the number of clients."
             )
 
         if self.weights is not None and max(self.weights) > 1:
-            raise ValueError(
+            raise InvalidConfig(
                 "Provided weights contains an element greater than 1, we do not allow sampling more than one time the entire dataset per client."
             )
         if self.weights is not None and min(self.weights) < 0:
-            raise ValueError(
+            raise InvalidConfig(
                 "Provided weights contains negative numbers, we do not allow that."
             )
 
     def __validate_classes_per_client(self):
         if isinstance(self.classes_per_client, tuple):
             if len(self.classes_per_client) != 2:
-                raise ValueError(
+                raise InvalidConfig(
                     f"classes_per_client if provided as a tuple, it must have two elements, mininum number of classes per client and maximum number of classes per client, but classes_per_client={self.classes_per_client}."
                 )
         elif not isinstance(self.classes_per_client, int) and self.n_clients != len(
             self.classes_per_client
         ):
-            raise ValueError(
+            raise InvalidConfig(
                 "classes_per_client if provided as a list o np.ndarray, its length and n_clients must equal."
             )
 
-    def __validate_features_per_class(self):
+    def __validate_features_per_client(self):
+        if not self.replacement:
+            raise InvalidConfig(
+                "By setting replacement to False and specifying features_per_client, clients will not share any data instances."
+            )
         if isinstance(self.features_per_client, tuple):
             if len(self.features_per_client) != 2:
-                raise ValueError(
+                raise InvalidConfig(
                     f"features_per_client if provided as a tuple, it must have two elements, mininum number of features per client and maximum number of features per client, but features_per_client={self.features_per_client}."
                 )
         elif not isinstance(self.features_per_client, int) and self.n_clients != len(
             self.features_per_client
         ):
-            raise ValueError(
+            raise InvalidConfig(
                 "features_per_client if provided as a list o np.ndarray, its length and n_clients must equal."
             )
