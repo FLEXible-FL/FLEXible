793c96cdb3427f4e8ae8b2012d0fec879b5b1f31
Alberto Argente del Castillo Garrido <albertoargentedelcastillogarrido@MacBook-Pro-de-Alberto.local>
2022-12-05 08:43:34 +0100

    Documentation theme changed to spinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ author = "FLEXible Team"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -41,5 +41,6 @@ source_suffix = ".rst"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+# html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
