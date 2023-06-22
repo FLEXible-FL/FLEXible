0749a54df0c2defab0018c5edf73aae60eb48893
AlArgente <albertoargentedcgarrido@gmail.com>
2023-06-22 22:36:56 +0200

    Linter applied

--- flex/pool/decorators.py
+++ flex/pool/decorators.py
@@ -1,8 +1,9 @@
-import inspect
 import functools
-
+import inspect
 from typing import List
+
 from flex.model import FlexModel
+
 
 def __inspect_arguments(func):
     signature = inspect.signature(func)
@@ -13,10 +14,12 @@ def __inspect_arguments(func):
             f"The function must recieve at least 1 argument. {er}"
         ) from er
 
+
 def init_server_model(func):
     __inspect_arguments(func=func)
+
     @functools.wraps(func)
-    def _init_server_model_(server_flex_model:FlexModel, _, *args, **kwargs):
+    def _init_server_model_(server_flex_model: FlexModel, _, *args, **kwargs):
         server_flex_model.update(func(*args, **kwargs))
 
     return _init_server_model_
@@ -24,8 +27,14 @@ def init_server_model(func):
 
 def deploy_server_model(func):
     __inspect_arguments(func=func)
+
     @functools.wraps(func)
-    def _deploy_model_(server_flex_model:FlexModel, clients_flex_models:List[FlexModel], *args, **kwargs):
+    def _deploy_model_(
+        server_flex_model: FlexModel,
+        clients_flex_models: List[FlexModel],
+        *args,
+        **kwargs,
+    ):
         for k in clients_flex_models:
             # Reminder, it is not possible to make assignements here
             clients_flex_models[k].update(func(server_flex_model, *args, **kwargs))
@@ -35,8 +44,14 @@ def deploy_server_model(func):
 
 def collect_clients_weights(func):
     __inspect_arguments(func=func)
+
     @functools.wraps(func)
-    def _collect_weights_(aggregator_flex_model:FlexModel, clients_flex_models:List[FlexModel], *args, **kwargs):
+    def _collect_weights_(
+        aggregator_flex_model: FlexModel,
+        clients_flex_models: List[FlexModel],
+        *args,
+        **kwargs,
+    ):
         if "weights" not in aggregator_flex_model:
             aggregator_flex_model["weights"] = []
         for k in clients_flex_models:
@@ -48,8 +63,9 @@ def collect_clients_weights(func):
 
 def aggregate_weights(func):
     __inspect_arguments(func=func)
+
     @functools.wraps(func)
-    def _aggregate_weights_(aggregator_flex_model:FlexModel, _, *args, **kwargs):
+    def _aggregate_weights_(aggregator_flex_model: FlexModel, _, *args, **kwargs):
         aggregator_flex_model["aggregated_weights"] = func(
             aggregator_flex_model["weights"], *args, **kwargs
         )
@@ -60,16 +76,20 @@ def aggregate_weights(func):
 
 def set_aggregated_weights(func):
     __inspect_arguments(func=func)
+
     @functools.wraps(func)
     def _deploy_aggregated_weights_(
-        aggregator_flex_model:FlexModel, servers_flex_models:FlexModel, *args, **kwargs
+        aggregator_flex_model: FlexModel,
+        servers_flex_models: FlexModel,
+        *args,
+        **kwargs,
     ):
         for k in servers_flex_models:
             func(
                 servers_flex_models[k],
                 aggregator_flex_model["aggregated_weights"],
                 *args,
-                **kwargs
+                **kwargs,
             )
 
     return _deploy_aggregated_weights_
@@ -77,8 +97,9 @@ def set_aggregated_weights(func):
 
 def evaluate_server_model(func):
     __inspect_arguments(func=func)
+
     @functools.wraps(func)
-    def _evaluate_server_model_(server_flex_model:FlexModel, _, *args, **kwargs):
+    def _evaluate_server_model_(server_flex_model: FlexModel, _, *args, **kwargs):
         return func(server_flex_model, *args, **kwargs)
 
     return _evaluate_server_model_
