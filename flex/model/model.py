64d07a3e1cbcef944fa1313f5f0c5ce8d5dd6037
AlArgente <albertoargentedcgarrido@gmail.com>
2023-06-27 13:32:02 +0200

    Modified FlexModel and its tests

--- flex/model/model.py
+++ flex/model/model.py
@@ -1,42 +1,10 @@
-from copy import deepcopy
-from typing import Any, Hashable
+import collections
 
 
-class FlexModel(dict):
+class FlexModel(collections.UserDict):
     """Class that represents a model owned by each node in a Federated Experiment.
-    It is important to note that the model key can only be used once.
+    The model must be storaged using the key 'model' because of the decorators,
+    as we assume that the model will be using that key.
     """
 
-    one_use_keys = ["model"]
-
-    def __setitem__(self, key: Hashable, value: Any) -> None:
-        if key in self.one_use_keys and key in self:
-            raise KeyError(f"{key} key can not be overwritten.")
-        else:
-            dict.__setitem__(self, key, value)
-
-    def __getattr__(self, key: Hashable):
-        try:
-            return self[key]
-        except KeyError as k:
-            raise KeyError(k) from k
-
-    def __setattr__(self, key: Hashable, value: Any):
-        if key in self.one_use_keys and key in self:
-            raise KeyError(f"{key} key can not be overwritten.")
-        else:
-            self[key] = value
-
-    def __delattr__(self, key: Hashable):
-        try:
-            del self[key]
-        except KeyError as k:
-            raise KeyError(k) from k
-
-    def __deepcopy__(self, memo):
-        cls = self.__class__
-        result = cls.__new__(cls)
-        memo[id(self)] = result
-        for k, v in self.items():
-            setattr(result, k, deepcopy(v, memo))
-        return result
+    pass
