88be0c3cc836bcc45ee3d2ab717a79bc8d85c7bc
xehartnort <xehartnort@gmail.com>
2023-10-09 11:41:49 +0200

    add weighted fed_avg

--- flex/pool/aggregators.py
+++ flex/pool/aggregators.py
@@ -45,7 +45,7 @@ def set_tensorly_backend(
 
 def fed_avg_f(aggregated_weights_as_list: list):
     n_clients = len(aggregated_weights_as_list)
-    ponderation = [1.]*n_clients
+    ponderation = [1.0] * n_clients
     return weighted_fed_avg_f(aggregated_weights_as_list, ponderation)
 
 
@@ -67,16 +67,16 @@ def weighted_fed_avg_f(aggregated_weights_as_list: list, ponderation: list):
 
 @aggregate_weights
 def fed_avg(aggregated_weights_as_list: list):
-    """Function that implements de FedAvg aggregation method
+    """Function that implements the FedAvg aggregation method
 
     Args:
         aggregated_weights_as_list (list): List which contains
         all the weights to aggregate
 
     Returns:
-        np.array: An array with the aggregated weights
+        tensor array: An array with the aggregated weights
 
-    Example of use assuming you are using a client-server architechture:
+    Example of use assuming you are using a client-server architecture:
 
         from flex.pool.primitive_functions import fed_avg
 
@@ -85,7 +85,7 @@ def fed_avg(aggregated_weights_as_list: list):
         aggregator.map(server, fed_avg)
 
     Example of use using the FlexPool without separating server
-    and aggregator, and following a client-server architechture.
+    and aggregator, and following a client-server architecture.
 
         from flex.pool.primitive_functions import fed_avg
 
@@ -97,30 +97,33 @@ def fed_avg(aggregated_weights_as_list: list):
 
 @aggregate_weights
 def weighted_fed_avg(aggregated_weights_as_list: list, ponderation: list):
-    """Function that implements de FedAvg aggregation method
+    """Function that implements the weighted FedAvg aggregation method.
 
     Args:
+    ----
         aggregated_weights_as_list (list): List which contains
         all the weights to aggregate
+        ponderation (list): weights assigned to each client
 
     Returns:
-        np.array: An array with the aggregated weights
+    -------
+        tensor array: An array with the aggregated weights
 
-    Example of use assuming you are using a client-server architechture:
+    Example of use assuming you are using a client-server architecture:
 
-        from flex.pool.primitive_functions import fed_avg
+        from flex.pool.primitive_functions import weighted_fed_avg
 
         aggregator = flex_pool.aggregators
         server = flex_pool.servers
-        aggregator.map(server, fed_avg)
+        dummy_poderation = [1.]*len(flex_pool.clients)
+        aggregator.map(server, weighted_fed_avg, ponderation=dummy_poderation)
 
     Example of use using the FlexPool without separating server
-    and aggregator, and following a client-server architechture.
+    and aggregator, and following a client-server architecture.
 
-        from flex.pool.primitive_functions import fed_avg
-
-        flex_pool.aggregators.map(flex_pool.servers, fed_avg)
+        from flex.pool.primitive_functions import weighted_fed_avg
+        dummy_poderation = [1.]*len(flex_pool.clients)
+        flex_pool.aggregators.map(flex_pool.servers, weighted_fed_avg, ponderation=dummy_poderation)
     """
     set_tensorly_backend(aggregated_weights_as_list)
     return weighted_fed_avg_f(aggregated_weights_as_list, ponderation)
-
